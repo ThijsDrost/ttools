101cf1678b14a14bd01135f653e5e431fd64c710
ThijsDrost <120206360+ThijsDrost@users.noreply.github.com>
2025-07-07 16:14:46 +0200

    Make sorter more generic (Iterable instead of Collection)

--- ttools/sort.py
+++ ttools/sort.py
@@ -1,14 +1,22 @@
-def sort_by(sorter, *to_sort, key=None, reverse=False) -> tuple[tuple, ...]:
+from typing import Iterable
+
+from ._protecols import SupportsRichComparisonT
+
+def sort_by(sorter: Iterable[SupportsRichComparisonT], *to_sort: Iterable, key=None, reverse=False) -> tuple[tuple, ...]:
+    """
+    Sort several sequences together based on a sorter sequence.
+    """
     values = sort_together(sorter, *to_sort, key=key, reverse=reverse)
     return values[1:]
 
 
-def sort_together(sorter, *others, key=None, reverse=False) -> tuple[tuple, ...]:
-    if not all(len(sorter) == len(other) for other in others):
-        raise ValueError('The lengths of the lists must be the same.')
+def sort_together(sorter: Iterable[SupportsRichComparisonT], *others: Iterable, key=None, reverse=False) -> tuple[tuple, ...]:
     if key is not None:
         key_sorter = lambda x: key(x[0])
     else:
         key_sorter = lambda x: x[0]
-    sorted_values = sorted(zip(sorter, *others), key=key_sorter, reverse=reverse)
+    try:
+        sorted_values = sorted(zip(sorter, *others, strict=True), key=key_sorter, reverse=reverse)
+    except ValueError as e:
+        raise ValueError('The lengths of the lists must be the same.') from e
     return tuple(zip(*sorted_values))
