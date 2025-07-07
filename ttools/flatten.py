c652ac4be22f32c48072aba76222e2871cc5f712
ThijsDrost <120206360+ThijsDrost@users.noreply.github.com>
2025-07-07 16:13:33 +0200

    Add max depth to flatten

--- ttools/flatten.py
+++ ttools/flatten.py
@@ -1,32 +1,51 @@
 from typing import Iterable, overload, Any
 import functools
 import operator
+import math
 
 from _protecols import Addable
 
-def flatten_2D[T](value: Iterable[Iterable[Any]], /) -> list[Any]:
+def flatten_2D(value: Iterable[Iterable[Any]], /) -> list[Any]:
     """
     Flatten an iterator of iterators
     """
     return [item for sublist in value for item in sublist]
 
 
-def flatten_iter(value: Iterable, /) -> Iterable:
+def flatten_iter(value: Iterable, /, *, max_dept=math.inf) -> Iterable:
     """
     Flatten an arbitrary depth iterator
+
+    Parameters
+    ----------
+    value : Iterable
+        The iterable to flatten.
+    max_dept : int, optional
+        The maximum depth to flatten. Defaults to infinity, meaning it will flatten all levels.
     """
+    if max_dept <= 0:
+        yield value
+        return
+
     for item in value:
         if isinstance(item, Iterable):
-            yield from flatten_iter(item)
+            yield from flatten_iter(item, max_dept=max_dept-1)
         else:
             yield item
 
 
-def flatten(value: Iterable, /) -> list:
+def flatten(value: Iterable, /, *, max_dept=math.inf) -> list:
     """
     Flatten an arbitrary depth iterator
+
+    Parameters
+    ----------
+    value : Iterable
+        The iterable to flatten.
+    max_dept : int, optional
+        The maximum depth to flatten. Defaults to infinity, meaning it will flatten all levels.
     """
-    return list(flatten_iter(value))
+    return list(flatten_iter(value, max_dept=max_dept))
 
 
 def transpose(iterable: Iterable[Iterable[Any]], /) -> list[list[Any]]:
