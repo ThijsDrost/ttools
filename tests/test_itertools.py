2f792aa67830efc138ff7d0b0c5c0f883d9b6ead
ThijsDrost <120206360+ThijsDrost@users.noreply.github.com>
2025-07-31 21:29:53 +0200

    Add tests for the min-max and sorting functions

--- tests/test_itertools.py
+++ tests/test_itertools.py
@@ -1,8 +1,11 @@
 import sys
+import random
 
 sys.path.append(".")  # Adjust the path to import from the parent directory
 
 from ttools.itertools import sim_product
+from ttools.min_max import *
+from ttools.sort import sort_by
 
 def test_sim_product():
     num = 20
@@ -17,3 +20,36 @@ def test_sim_product():
                 if len(result) != i * j * k:
                     msg = f"{i} * {j} * {k} = {i * j * k}:\nS: {len(result)}, {result}\n"
                     raise ValueError(msg)
+
+
+def test_min_max():
+    for _ in range(10):
+        values = [random.randint(0, 100) for _ in range(100)]
+
+        max_value = max(values)
+        min_value = min(values)
+        assert(max_value == values[arg_max(values)])
+        assert(min_value == values[arg_min(values)])
+        assert(max_value == values[arg_none_max(values)])
+        assert(min_value == values[arg_none_min(values)])
+
+        for i in range(20):
+            values[random.randint(0, 99)] = None
+
+        max_value = values[arg_none_max(values)]
+        min_value = values[arg_none_min(values)]
+        assert(all(x is None or x <= max_value for x in values))
+        assert(all(x is None or min_value <= x for x in values))
+
+        max_value = none_max(values)
+        min_value = none_min(values)
+        assert(all(x is None or x <= max_value for x in values))
+        assert(all(x is None or min_value <= x for x in values))
+
+
+def test_sort():
+    values = list(range(100))
+    vals = values.copy()
+    random.shuffle(values)
+    val2 = sort_by(values, values.copy())[0]
+    assert(all(x == y for x, y in zip(vals, val2)))
